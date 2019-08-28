ce9f0f39e2f0945d778eab9e7f9ef859b68720fd
hasanNurubuloev <hasan.nurubuloev59@gmail.com>
2019-08-28 23:08:50 +0600

    основное+на сообразительность

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import javax.lang.model.type.ArrayType;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,18 +10,28 @@ public class Main {
        names [0] = "Jack";
        names [1] = "John";
        names [2] = "Bill";
-        for (int i = 1; i <names.length ; i++) {
-           switch (i) {
-               case 1:
-               System.out.println("Good morning, " + names[0]);
 
+       names = Arrays.copyOf (names, names.length +1);
+       names [names.length -1 ] = "Alex";
+
+        for (int i = 0; i <names.length ; i++) {
+           switch (i) {
+               case 0:
+               System.out.println("Good morning, " + names[0] + "!");
+                   break;
+
+               case 1:
+               System.out.println("Good afternoon, " + names[1] + "!");
+                   break;
                case 2:
-               System.out.println("Good afternoon, " + names[1]);
-               case 3:
-               System.out.println("Good evening, " + names[2]);
+               System.out.println("Good evening, " + names[2] + "!");
+                   break;
+                   case 3:
+               System.out.println("Good night, " + names[3] + "!");
+                   break;
 
            }
-            break;
+
         }
 
 
